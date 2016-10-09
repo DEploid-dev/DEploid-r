c7b2bbeb1491725a52bb55fc687c3ba24517c330
Joe Zhu <sha.joe.zhu@gmail.com>
2016-10-09 22:46:16 +0100

    update, and check install

--- R/dEploid.R
+++ R/dEploid.R
@@ -1,17 +1,1 @@
-#' blah blah
-#'
-#' blah blah blah
-#'
-#' @author
-#' Joe Zhu
-#'
-#' Maintainer: Joe Zhu \email{joezhu@well.ox.ac.uk}
-#' @name DEploid-package
-#' @docType package
-#'
-#' @importFrom Rcpp evalCpp
-#' @useDynLib DEploid
-#'
-#' @seealso
-#'
 NULL
--- buildLocally.sh
+++ buildLocally.sh
@@ -2,9 +2,9 @@
 # Building package
 R CMD build  .
 
-PKG_TARBALL=dEploid_0.3.tar.gz
+PKG_TARBALL=DEploid_0.3.1.tar.gz
 # Checking package
-R CMD check "${PKG_TARBALL}" --as-cran; CHECK_RET=$?
+R CMD check "${PKG_TARBALL}" --as-cran; CHECK_RET=r-devel
 
 # R CMD check fail logs
 $ for name in $(find "${RCHECK_DIR}" -type f -name "*fail");do echo ">>> Filename: ${name} <<<";cat ${name};done
