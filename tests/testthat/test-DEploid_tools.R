52163d348b81edc0097388b98d23deff3e8019d6
Joe Zhu <sha.joe.zhu@gmail.com>
2016-11-14 22:50:54 +0000

    update lint
    rework on coding style
    
    update .lintr

--- tests/testthat/test-DEploid_tools.R
+++ tests/testthat/test-DEploid_tools.R
@@ -1,30 +1,30 @@
 context("DEploid tools")
 
-vcfFile = system.file("extdata", "PG0390-C.test.vcf.gz", package = "DEploid")
-plafFile = system.file("extdata", "labStrains.test.PLAF.txt", package = "DEploid")
-panelFile = system.file("extdata", "labStrains.test.panel.txt", package = "DEploid")
-refFile = system.file("extdata", "PG0390-C.test.ref", package = "DEploid")
-altFile = system.file("extdata", "PG0390-C.test.alt", package = "DEploid")
+vcfFileName <- system.file("extdata", "PG0390-C.test.vcf.gz",
+    package = "DEploid")
+plafFileName <- system.file("extdata", "labStrains.test.PLAF.txt",
+    package = "DEploid")
+panel_file <- system.file("extdata", "labStrains.test.panel.txt",
+    package = "DEploid")
+ref_file <- system.file("extdata", "PG0390-C.test.ref", package = "DEploid")
+alt_file <- system.file("extdata", "PG0390-C.test.alt", package = "DEploid")
 
-test_that("Extracted coverage",
-{
-    PG0390CoverageTxt = extractCoverageFromTxt(refFile, altFile)
+test_that("Extracted coverage", {
+    PG0390CoverageTxt <- extractCoverageFromTxt(ref_file, alt_file)
     expect_that(PG0390CoverageTxt, is_a("data.frame"))
-    PG0390CoverageVcf = extractCoverageFromVcf(vcfFile)
+    PG0390CoverageVcf <- extractCoverageFromVcf(vcfFileName)
     expect_that(PG0390CoverageVcf, is_a("data.frame"))
     expect_equal(PG0390CoverageTxt, PG0390CoverageVcf)
 })
 
 
-test_that("Extracted plaf",
-{
-    plaf = extractPLAF(plafFile)
+test_that("Extracted plaf", {
+    plaf <- extractPLAF(plafFileName)
     expect_that(plaf, is_a("numeric"))
 })
 
 
-test_that("computeObsWSAF",
-{
+test_that("computeObsWSAF", {
     expect_equal(computeObsWSAF(0, 0), 0)
     expect_equal(computeObsWSAF(0, 100), 0)
     expect_equal(computeObsWSAF(1, 99), 0.01)
@@ -34,9 +34,9 @@ test_that("computeObsWSAF",
 })
 
 
-test_that("histWF",
-{
-    PG0390CoverageVcf = extractCoverageFromVcf(vcfFile)
-    obsWSAF = computeObsWSAF(PG0390CoverageVcf$altCount, PG0390CoverageVcf$refCount)
+test_that("histWF", {
+    PG0390CoverageVcf <- extractCoverageFromVcf(vcfFileName)
+    obsWSAF <- computeObsWSAF(PG0390CoverageVcf$altCount,
+        PG0390CoverageVcf$refCount)
     expect_that(histWSAF(obsWSAF), is_a("histogram"))
 })
