37d0f94c3d535bdb943a1d159adfb54e101bb5a3
Joe Zhu <sha.joe.zhu@gmail.com>
2016-12-10 09:27:24 +0000

    working progress

--- R/DEploidR.R
+++ R/DEploidR.R
@@ -134,7 +134,8 @@ plotProportions <- function (proportions, title = "Components"){
     rainbowColorBin <- 16
     barplot(t(proportions), beside = F, border = NA,
         col = rainbow(rainbowColorBin), space = 0, xlab = "Iteration",
-        ylab = "Component proportion", main = title)
+        ylab = "Component proportion", main = title, cex.lab = 2.5,
+        cex.main = 2.5, cex.axis = 2.5)
 }
 
 
@@ -168,9 +169,18 @@ plotProportions <- function (proportions, title = "Components"){
 #'
 plotAltVsRef <- function ( ref, alt, title = "Alt vs Ref",
                     exclude.ref = c(), exclude.alt = c() ){
+    cr <- colorRampPalette(colors=c("#de2d26","#2b8cbe"))
+    colors <- cr(31)
+    ratios <- ref/(ref+alt+0.0000001)
     tmpRange <- 1.1 * mean(max(alt), max(ref))
     plot ( ref, alt, xlim = c(0, tmpRange), ylim = c(0, tmpRange),
-        cex = 0.5, xlab = "REF", ylab = "ALT", main = title)
+        pch=20, col=scales::alpha(colors[ceiling(ratios * 30)+1], 0.7),
+        xlab="Reference # Reads", ylab="Alternative # Reads", main=title,
+        cex = 0.5, cex.lab = 2.5, cex.main = 2.5, cex.axis = 2.5)
+    legend("topright", legend=c("100% Alt","100% Ref", "50/50"),
+        fill=colors[c(1,31,15)], cex=2.5, border=NA, box.lwd=0, box.col="white",
+        bg = NA)
+    abline(a=0,b=1, lwd=2, lty=2, col="gray")
     points (exclude.ref, exclude.alt, col = "red")
     abline(v = 50, untf = FALSE, lty = 2)
     abline(h = 50, untf = FALSE, lty = 2)
@@ -217,7 +227,8 @@ histWSAF <- function ( obsWSAF, exclusive = TRUE,
         tmpWSAFIndex <- which( ( (obsWSAF < 1) * (obsWSAF > 0) ) == 1)
     }
     return (hist(obsWSAF[tmpWSAFIndex], main = title,
-        breaks = seq(0, 1, by = 0.1), xlab = "WSAF"))
+        breaks = seq(0, 1, by = 0.1), xlab = "WSAF", col="gray", cex.lab = 2.5,
+        cex.main = 2.5, cex.axis = 2.5))
 }
 
 
@@ -257,7 +268,8 @@ histWSAF <- function ( obsWSAF, exclusive = TRUE,
 plotWSAFvsPLAF <- function ( plaf, obsWSAF, expWSAF = c(),
                          title = "WSAF vs PLAF" ){
     plot ( plaf, obsWSAF, cex = 0.5, xlim = c(0, 1), ylim = c(0, 1),
-        col = "red", main = title, xlab = "PLAF", ylab = "WSAF" )
+        col = "red", main = title, xlab = "PLAF", ylab = "WSAF", cex.lab = 2.5,
+        cex.main = 2.5, cex.axis = 2.5 )
     if ( length(expWSAF) > 0 ){
         points ( plaf, expWSAF, cex = 0.5, col = "blue")
     }
@@ -291,7 +303,8 @@ plotObsExpWSAF <- function (obsWSAF, expWSAF,
                       title = "WSAF(observed vs expected)"){
     plot(obsWSAF, expWSAF, pch = 19, col = "blue",
         xlab = "Observed WSAF (ALT/(ALT+REF))", ylab = "Expected WSAF (h%*%p)",
-        main = title, xlim = c(-0.05, 1.05), cex = 0.5, ylim = c(-0.05, 1.05));
+        main = title, xlim = c(-0.05, 1.05), cex = 0.5, ylim = c(-0.05, 1.05),
+        cex.lab = 2.5, cex.main = 2.5, cex.axis = 2.5);
     abline(0, 1, lty = "dotted");
 
 }
@@ -338,9 +351,10 @@ computeObsWSAF <- function (alt, ref) {
 #'
 #' @export
 #'
-haplotypePainter <- function (posteriorProbabilities, title = ""){
+haplotypePainter <- function (posteriorProbabilities, title = "", labelScaling){
     rainbowColorBin <- 16
     barplot(t(posteriorProbabilities), beside = F, border = NA,
         col = rainbow(rainbowColorBin), space = 0, xlab = "SNP index",
-        ylab = "Posterior probabilities", main = title)
+        ylab = "", main = title, cex.axis = labelScaling/5, cex.lab = labelScaling/6,
+        cex.main = labelScaling/4)
 }
